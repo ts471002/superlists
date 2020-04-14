063ddd851e6ee20e0f433784a397b2cc1043d6b1
Hanoi <502660314@qq.com>
2020-04-14 09:29:41 +0800

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,23 +1,33 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolve_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+    # def test_root_url_resolve_to_home_page_view(self):
+    #     found = resolve('/')
+    #     self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertEqual(html.startswith('<html>'), True)
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertEqual(html.endswith('</html>'), True)
+    # def test_home_page_returns_correct_html(self):
+    #     # request = HttpRequest()
+    #     # response = home_page(request)
+    #     response = self.client.get('/')
+
+    #     html = response.content.decode('utf8')
+    #     # expected_html = render_to_string('home.html')
+    #     # self.assertEqual(html, expected_html)
+    #     self.assertEqual(html.startswith('<html>'), True)
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertEqual(html.endswith('</html>'), True)
+
+    #     self.assertTemplateUsed(response, 'home.html')
 
 
 # Create your tests here.
