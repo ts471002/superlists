d9c804507f61d8e099791c577f06a7a163ba0e4f
Hanoi <502660314@qq.com>
2020-04-21 11:07:16 +0800

    a new commit

--- lists/tests.py
+++ lists/tests.py
@@ -11,23 +11,10 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-    # def test_root_url_resolve_to_home_page_view(self):
-    #     found = resolve('/')
-    #     self.assertEqual(found.func, home_page)
 
-    # def test_home_page_returns_correct_html(self):
-    #     # request = HttpRequest()
-    #     # response = home_page(request)
-    #     response = self.client.get('/')
-
-    #     html = response.content.decode('utf8')
-    #     # expected_html = render_to_string('home.html')
-    #     # self.assertEqual(html, expected_html)
-    #     self.assertEqual(html.startswith('<html>'), True)
-    #     self.assertIn('<title>To-Do lists</title>', html)
-    #     self.assertEqual(html.endswith('</html>'), True)
-
-    #     self.assertTemplateUsed(response, 'home.html')
-
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 
 # Create your tests here.
